1866d4927bc408cf6ddcadfaf9288626ad0f5415
khayyon1 <k_parker@coloradocollege.edu>
2019-08-20 11:33:06 -0400

    Added Division Function and Test

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ Unit tests for the calculator library
 '''
 
 
-from calculator import add, subtract, multiply
+from calculator import add, subtract, multiply, divide
 
 
 class TestCalculator:
@@ -18,3 +18,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 90 == multiply(10, 9)
+
+    def test_division(self):
+        assert 1 == divide(1, 1)
